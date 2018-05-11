69dc329c4633bb1df7c4f25b8d938737bc3205e9
Alexis Bell <abell@techtonicgroup.com>
2018-05-11 12:54:46 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,70 @@
+let QuestionMarks = str => {
+  let numberIndexes = [];
+  let numberValues = [];
+  for(c in str)
+    {
+      if(isNumber(str[c]))
+        {
+          numberIndexes.push(parseInt(c));
+          numberValues.push(parseInt(str[c]));
+        }
+    }
+  if(numberIndexes.length < 1)
+    {
+      return false;
+    } 
+  var equalsTen = false;
+  for(var i = 0; i < numberIndexes.length -1; i++)
+    {
+      if(findValue(numberValues[i], numberValues[i+1])){
+        equalsTen = true;
+        var hasThree = str.slice((numberIndexes[i] + 1), numberIndexes[i+1]);
+        if(!threeQuestionsMarks(hasThree))
+        {
+          return false;
+        }
+      }
+    }
+    return equalsTen;
+};
+
+let isNumber = c => {
+  if( (c * 0) == 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+let findValue = (n1, n2) =>{
+  if((n1 + n2) === 10)
+    {
+      return true;
+    } else {
+      return false;
+    }
+}
+let threeQuestionsMarks = substr =>{
+  var counter = 0;
+  for(c in substr)
+    {
+      if(substr[c] === '?')
+        {
+          counter++;
+        } 
+    }
+  if(counter === 3)
+    {
+      return true;
+    } else{
+      return false;
+    }
+}
+console.time('one');
+console.log(QuestionMarks('bcc?7??ccc?3tt1??????5'));
+console.timeEnd('one');
+console.time('two');
+console.log(QuestionMarks('bb6?9'));
+console.timeEnd('two');
+console.time('three');
+console.log(QuestionMarks('arrb6???4xxbl5???eee5'));
+console.timeEnd('three');
